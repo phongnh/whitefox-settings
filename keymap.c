1ef5452dcd413125d1fb2eb14acdaa1dcb35d413
Phong Nguyen <nhphong1406@gmail.com>
2018-04-04 08:29:45 +0700

    Redefines new Whitefox mappings

--- keymap.c
+++ keymap.c
@@ -18,8 +18,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 enum custom_layers {
     _BASE,
-    _MEDIA,
-    _SYMBOL,
+    _LFN,
+    _RFN,
     _MOUSE,
 };
 
@@ -30,64 +30,64 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------------------------------------------------------------------------------|
      * |Tab   |  Q |  W |  E |  R |  T |  Y |  U |  I |  O |  P |  [ |  ] |   \  | Del|
      * |------------------------------------------------------------------------------|
-     * |FN     |  A |  S |  D |  F |  G |  H |  J |  K |  L | ;  |  ' |     Enter|PgUp|
+     * |Ctrl   |  A |  S |  D |  F |  G |  H |  J |  K |  L | ;  |  ' |     Enter|PgUp|
      * |------------------------------------------------------------------------------|
      * |Shift    |  Z |  X |  C |  V |  B |  N |  M |  , |  . |  / |  Shift | Up |PgDn|
      * |------------------------------------------------------------------------------|
-     * |Ctrl | Gui | Alt |             Space            |Alt |Menu|Ctrl|Left|Down|Rght|
+     * |FN   | Gui | Alt |             Space            |Alt |Gui | FN |Left|Down|Rght|
      * `------------------------------------------------------------------------------'
      */
     [_BASE] = KEYMAP( \
-        KC_ESC,              KC_1,     KC_2,          KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,     KC_0,                 KC_MINS,          KC_EQL,   KC_BSPC,             KC_BSPC,  KC_GRV,   \
-        KC_TAB,              KC_Q,     KC_W,          KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,     KC_P,                 KC_LBRC,          KC_RBRC,  KC_BSLS,                       KC_DEL,   \
-        LT(_MEDIA, KC_ESC),  KC_A,     KC_S,          KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,     LT(_MOUSE, KC_SCLN),  ALT_T(KC_QUOTE),  KC_NUHS,  LT(_SYMBOL, KC_ENT),           KC_PGUP,  \
-        KC_LSFT,             KC_NUBS,  LCTL_T(KC_Z),  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,  KC_DOT,               RCTL_T(KC_SLSH),  KC_RSFT,  KC_UP,                         KC_PGDN,  \
-        KC_LCTL,             KC_LGUI,  KC_LALT,                       KC_SPC,                   KC_RALT,  RGUI_T(KC_APP),       KC_RCTL,          KC_LEFT,  KC_DOWN,                       KC_RGHT   \
+        KC_ESC,            KC_1,     KC_2,     KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,     KC_0,                 KC_MINS,           KC_EQL,   KC_BSPC,            KC_BSPC,  KC_GRV,   \
+        LT(_LFN, KC_TAB),  KC_Q,     KC_W,     KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,     KC_P,                 KC_LBRC,           KC_RBRC,  LT(_RFN, KC_BSLS),            KC_DEL,   \
+        CTL_T(KC_ESC),     KC_A,     KC_S,     KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,     LT(_MOUSE, KC_SCLN),  ALT_T(KC_QUOTE),   KC_NUHS,  RCTL_T(KC_ENT),               KC_PGUP,  \
+        KC_LSFT,           KC_NUBS,  KC_Z,     KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,  KC_DOT,               KC_SLSH,           KC_RSFT,  KC_UP,                        KC_PGDN,  \
+        LT(_LFN, KC_GRV),  KC_LGUI,  KC_LALT,                  KC_SPC,                   KC_RALT,  KC_RGUI,              LT(_RFN, KC_APP),  KC_LEFT,  KC_DOWN,                      KC_RGHT   \
     ),
 
-    /* Layer 1: Media (and Symbol) Layer
+    /* Layer 2: Left Function Layer
     * ,------------------------------------------------------------------------------.
-    * |SLEP| F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8 | F9 |F10 |F11 |F12 |        |BTLD|
+    * |    | F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8 | F9 |F10 |F11 |F12 |  RALT  |PSCR|
     * |------------------------------------------------------------------------------|
-    * |WAKE  | F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8 | F9 |F10 |F11 |F12 |PrtScr| Ins|
+    * |      | F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8 | F9 |F10 |F11 |F12 | RGUI |Ins |
     * |------------------------------------------------------------------------------|
-    * |       |PREV|PLAY|NEXT|    |    |    |VOLD|VOLU|    |    |    |  Control |Home|
+    * |       |    |    |    |    |    |    |    |    |Led |    |    |   RCTL   |Home|
     * |------------------------------------------------------------------------------|
-    * |         |    |    |    |    |    |NORM|SWAP|F14 |F15 |LED |        |    |End |
+    * |         |    |    |    |    |    |Norm|Swap|Dec |Inc |Step|        |VolU| End|
     * |------------------------------------------------------------------------------|
-    * |     |     |     |                              |    |    |    |    |    |    |
+    * |     |     |     |                              |    |    |    |F14 |VolD| F15|
     * `------------------------------------------------------------------------------'
     */
-    [_MEDIA] = KEYMAP( \
-        KC_SLEP,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,  KC_TRNS,  RESET,    \
-        KC_WAKE,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,            KC_INS,   \
-        KC_TRNS,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_VOLD,  KC_VOLU,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_RCTL,            KC_HOME,  \
-        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  AG_NORM,  AG_SWAP,  KC_F14,   KC_F15,   BL_STEP,  KC_TRNS,  KC_TRNS,            KC_END,   \
-        KC_TRNS,  KC_TRNS,  KC_TRNS,                           KC_TRNS,                           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS   \
+    [_LFN] = KEYMAP( \
+        KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_RALT,  KC_TRNS,  KC_PSCR,  \
+        KC_TRNS,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_RGUI,            KC_INS,   \
+        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BL_TOGG,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_HOME,  \
+        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  AG_NORM,  AG_SWAP,  BL_DEC,   BL_INC,   BL_STEP,  KC_TRNS,  KC_VOLU,            KC_END,   \
+        KC_TRNS,  KC_TRNS,  KC_TRNS,                           KC_TRNS,                           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_SLCK,  KC_VOLD,            KC_PAUS   \
     ),
 
-    /* Layer 2: Media (and Symbol) Layer
+    /* Layer 3: Right Function Layer
     * ,------------------------------------------------------------------------------.
-    * |SLEP| F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8 | F9 |F10 |F11 |F12 |        |BTLD|
+    * |LALT| F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8 | F9 |F10 |F11 |F12 |        |PSCR|
     * |------------------------------------------------------------------------------|
-    * |WAKE  | F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8 | F9 |F10 |F11 |F12 |PrtScr| Ins|
+    * |LGUI  | F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8 | F9 |F10 |F11 |F12 |      |Ins |
     * |------------------------------------------------------------------------------|
-    * |Control|PREV|PLAY|NEXT|    |    |    |VOLD|VOLU|    |    |    |          |Home|
+    * |LCTL   |    |    |    |    |    |    |    |    |Led |    |    |          |Home|
     * |------------------------------------------------------------------------------|
-    * |         |    |    |    |    |    |NORM|SWAP|F14 |F15 |LED |        |    |End |
+    * |         |    |    |    |    |    |Norm|Swap|Dec |Inc |Step|        |VolU| End|
     * |------------------------------------------------------------------------------|
-    * |     |     |     |                              |    |    |    |    |    |    |
+    * |     |     |     |                              |    |    |    |F14 |VolD| F15|
     * `------------------------------------------------------------------------------'
     */
-    [_SYMBOL] = KEYMAP( \
-        KC_SLEP,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,  KC_TRNS,  RESET,    \
-        KC_WAKE,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,            KC_INS,   \
-        KC_LCTL,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_VOLD,  KC_VOLU,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_HOME,  \
-        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  AG_NORM,  AG_SWAP,  KC_F14,   KC_F15,   BL_STEP,  KC_TRNS,  KC_TRNS,            KC_END,   \
-        KC_TRNS,  KC_TRNS,  KC_TRNS,                           KC_TRNS,                           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS   \
+    [_RFN] = KEYMAP( \
+        KC_LALT,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,  KC_TRNS,  KC_PSCR,  \
+        KC_LGUI,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_TRNS,            KC_INS,   \
+        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BL_TOGG,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_HOME,  \
+        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  AG_NORM,  AG_SWAP,  BL_DEC,   BL_INC,   BL_STEP,  KC_TRNS,  KC_VOLU,            KC_END,   \
+        KC_TRNS,  KC_TRNS,  KC_TRNS,                           KC_TRNS,                           KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_F14,   KC_VOLD,            KC_F15    \
     ),
 
-    /* Layer 3: Mouse Layer
+    /* Layer 4: Mouse Layer
     * ,------------------------------------------------------------------------------.
     * |    |    |    |    |    |    |    |    |    |    |    |    |    |        |    |
     * |------------------------------------------------------------------------------|
@@ -101,7 +101,7 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * `------------------------------------------------------------------------------'
     */
     [_MOUSE] = KEYMAP( \
-        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  \
+        RESET,    KC_TRNS,  KC_FN1,   KC_FN2,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  \
         KC_TRNS,  KC_TRNS,  KC_BTN3,  KC_MS_U,  KC_BTN2,  KC_TRNS,  KC_TRNS,  KC_BTN2,  KC_WH_U,  KC_BTN3,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  \
         KC_CAPS,  KC_BTN1,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_BTN1,  KC_BTN1,  KC_WH_L,  KC_WH_D,  KC_WH_R,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  \
         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  \
@@ -109,3 +109,8 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 };
 
+const uint16_t PROGMEM fn_actions[] = {
+    [1] = ACTION_LAYER_TOGGLE(_LFN),
+    [2] = ACTION_LAYER_TOGGLE(_RFN),
+    [3] = ACTION_LAYER_TOGGLE(_MOUSE),
+};
